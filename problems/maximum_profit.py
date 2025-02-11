2dd35c206efa054840d0317ee41e394abb5e9346
martinsglucas <martin5.luca5.univ@gmail.com>
2025-02-10 22:22:36 -0300

    lida corretamente com indices (TLE)

--- problems/maximum_profit.py
+++ problems/maximum_profit.py
@@ -1,26 +1,28 @@
 class Solution:
+
     def max(self, take, dontTake):
-        if take >= dontTake:
-            return take
-        else:
-            return dontTake
+        return take if take >= dontTake else dontTake
+    
     def compatible(self, jobs, i):
         for j in range(i-1, -1, -1):
             if jobs[j][1] <= jobs[i][0]:
                 return j
-
+        return -1
+    
     def compatibles(self, jobs):
         p = []
-        for i in range(len(jobs) - 1, -1, -1):
+        for i in range(len(jobs)):
             p.append(self.compatible(jobs, i))
-        return list(reversed(p))
-
+        return p
 
     def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
         jobs = sorted(zip(startTime, endTime, profit), key=lambda x: x[1])
-        result = [-1 for job in jobs]
-        result[0] = 0
+        n = len(jobs)
+        result = [0] * n
         p = self.compatibles(jobs)
-        for i in range(1, len(jobs)):
-            result[i] = self.max(jobs[i][2] + result[p[i]],  result[i-1])
+        for i in range(n):
+            include_profit = jobs[i][2]
+            if p[i] != -1:
+                include_profit += result[p[i]]
+            result[i] = self.max(include_profit, result[i - 1] if i > 0 else 0)
         return result[-1]
