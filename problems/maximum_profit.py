2ce19d173ca065eae5be62e76866400e4acb5517
martinsglucas <martin5.luca5.univ@gmail.com>
2025-02-10 22:37:25 -0300

    adiciona solucao final

--- problems/maximum_profit.py
+++ problems/maximum_profit.py
@@ -6,6 +6,7 @@ class Solution:
     def compatible(self, jobs, i, end_times):
         j = bisect.bisect_right(end_times, jobs[i][0]) - 1
         return j
+
     def compatibles(self, jobs):
         end_times = [job[1] for job in jobs]
         return [self.compatible(jobs, i, end_times) for i in range(len(jobs))]
@@ -15,9 +16,12 @@ class Solution:
         n = len(jobs)
         result = [0] * n
         p = self.compatibles(jobs)
+        
         for i in range(n):
             include_profit = jobs[i][2]
             if p[i] != -1:
                 include_profit += result[p[i]]
+            
             result[i] = max(include_profit, result[i - 1] if i > 0 else 0)
+        
         return result[-1]
